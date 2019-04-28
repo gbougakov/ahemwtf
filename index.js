64066e1a1ff8c9d74c531aa63087351882708876
George Bougakov <g@bougakov.com>
2019-04-28 19:58:28 +0300

    lol

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const isBot = require('isbot')
 
 const app = express()
 
 app.get('*', (req, res) => {
+  if (!isBot(req.headers['user-agent'])) {
     res.send(200, '<p class="text-align: center">a domain for random shit and emails by <a href="https://bygeorgenet.me">George Bougakov</a></p>')
+  } else {
+    res.sendFile('ahem.gif')
+  }
 })
 
 module.exports = app
