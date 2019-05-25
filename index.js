8ecc5eec6a6e172d8c335d886c3aef8c70680802
George Bougakov <g@bougakov.com>
2019-05-25 14:13:51 +0300

    init

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const isBot = require('isbot')
 
 const app = express()
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   if (!isBot(req.headers['user-agent'])) {
-    res.send(200, '<p class="text-align: center">a domain for random shit and emails by <a href="https://bygeorgenet.me">George Bougakov</a></p>')
+    res.redirect('https://ahem.wtf/about')
   } else {
-    res.sendFile(__dirname + '/ahem.gif')
+    res.redirect(302, 'https://ahem.wtf/gifs/wtf' + (Math.floor(Math.random() * 7) + 1) + '.gif')
   }
 })
-
-app.listen(process.env.PORT)
+//app.listen(process.env.PORT
+app.listen()
+ 
